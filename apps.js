9c089dfb655d105a4083131c1a761d8b84bb05b7
Andres Puulinn <andres.puulinn@gmail.com>
2017-04-21 23:50:52 +0300

    add connetion to memento backend and requesting info

--- apps.js
+++ apps.js
@@ -1,3 +1,4 @@
+const net = require('net')
 const TelegramBot = require('node-telegram-bot-api')
 
 // Create a bot that uses 'polling' to fetch new updates
@@ -10,5 +11,21 @@ bot.onText(/\/version/, function (msg, match) {
 })
 
 bot.onText(/^[^\/].*/, function (msg, match) {
-    bot.sendMessage(msg.chat.id, "let's search <" + match + ">")
+    // bot.sendMessage(msg.chat.id, "let's search <" + match + ">")
+    const search = match[0]
+
+    const client = new net.Socket()
+    client.connect(process.env.MEMENTO_PORT, process.env.MEMENTO_HOST, function() {
+	    console.log('connected')
+	    client.write(search + "\n")
+    })
+
+    client.on('data', function(data) {
+	    //console.log('received: ' + data)
+        bot.sendMessage(msg.chat.id, data)
+    })
+
+    client.on('close', function() {
+	    console.log('connection closed')
+    })
 })
